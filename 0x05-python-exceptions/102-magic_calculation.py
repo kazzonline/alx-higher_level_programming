a3f344aa7c041503b2079886a969609f8697e2c8
kazzonline <azeezolatunji02@gmail.com>
2023-03-27 05:02:19 -0700

    102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -13,4 +12,3 @@ def magic_calculation(a, b):
             result = b + a
             break
     return (result)
-
