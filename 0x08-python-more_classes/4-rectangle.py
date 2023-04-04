79718fdc5b64726cdc4e8d7ee0531141389a9f5d
kazzonline <azeezolatunji02@gmail.com>
2023-04-04 07:39:29 -0700

    4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -23,22 +23,22 @@ class Rectangle:
         return ret[:-1]
 
     def __repr__(self):
-         """ repr """
-         return "Rectangle({}, {})".format(self.__width, self.__height)
+        """ repr """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
-     @property
-     def width(self):
-         """ width getter """
-         return self.__width
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
 
-     @width.setter
-     def width(self, value):
-         """ width setter """
-         if type(value) is not int:
-             raise TypeError("width must be an integer")
-         if value < 0:
-             raise ValueError("width must be >= 0")
-         self.__width = value
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -54,12 +54,12 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-     def area(self):
-         """ calculates area of a rectangle """
-         return self.__width * self.__height
+    def area(self):
+        """ calculates area of a rectangle """
+        return self.__width * self.__height
 
-     def perimeter(self):
-         """ calculates the perimeter of a rectangle """
-         if self.__height == 0 or self.__width == 0:
-             return 0
-         return 2 * (self.__width + self.__height)
+    def perimeter(self):
+        """ calculates the perimeter of a rectangle """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
