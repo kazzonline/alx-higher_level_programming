00d316e057ed2d0aa611183621058727227c2204
kazzonline <azeezolatunji02@gmail.com>
2023-04-12 09:50:44 -0700

    all tasks

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -18,5 +18,5 @@ class Student:
         self.age = age
 
     def to_json(self):
-         """Get a dictionary representation of the Student."""
-         return self.__dict__
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
